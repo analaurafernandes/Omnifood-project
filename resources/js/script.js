7b19691093ff8995519c5b6b00bb2247ff7e557b
analaurafernandes <analauraferoli@gmail.com>
2020-12-22 17:20:22 -0300

    Adding animations on buttons and while scrolling, a responsive menu navigation and favicons

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,8 +21,49 @@ $(document).ready(function(){
     
     $('.js--scroll-to-start').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    },{
+        offset: '50%'
     });
     
+    /*ANIMATIONS ON SCROLL*/
+    $('.js--wp-1').waypoint(function(direction){
+       $('.js--wp-1').addClass('animate__animated animate__fadeIn'); 
+    },{
+        offset: '50%'
+    });
     
+    $('.js--wp-2').waypoint(function(direction){
+       $('.js--wp-2').addClass('animate__animated animate__slideInUp'); 
+    },{
+        offset: '50%'
+    });
     
+    $('.js--wp-3').waypoint(function(direction){
+       $('.js--wp-3').addClass('animate__animated animate__fadeIn'); 
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
+    },{
+        offset: '50%'
+    })
+    
+    /*MOBILE NAVIGATION*/
+    
+    /*open and close nav*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon ion-icon')
+        nav.slideToggle(200); /*200 milliseconds*/
+        
+        if(icon.attr("name") === "menu-outline" ){ //if the attribute name of icon is == to menu-outline
+           icon.removeAttr("name"); //remove the name attribute
+           icon.attr("name","close-outline") // add the name attribute again and set it === to close-outline
+        }else {
+            icon.removeAttr("name","close-outline") //remove attribute name === close-outline
+            icon.attr("name","menu-outline") //add attribute name === menu-outline.
+        }
+    });
 });
